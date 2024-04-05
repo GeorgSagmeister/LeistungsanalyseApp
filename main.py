6977351138f7e80a9646ba2956f87da2f624913a
unknown <sg4751@mci4me.at>
2024-04-05 14:20:00 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 import my_functions
+import json
+from datetime import datetime
+
+
 
 if __name__ == "__main__":
+
+   # Datum fixieren
+    datum =  datetime.now().strftime("%d-%m-%Y")
+
+    # Experimentenname
+    name = input("Experimentenname:")
+    
     # Erstelle einen Betreuer
-    supervisor_info = my_functions.build_person("John", "Doe", "male", 40)
+    supervisor_info = my_functions.build_person("Georg", "sagmeister", "male", 20)
 
     # Erstelle einen Probanden
-    subject_info = my_functions.build_person("Jane", "Doe", "female", 35)
+    print("Bitte Daten der Versuchsperson eingeben:")
+    first_name = input("Vorname:")
+    last_name = input("Nachname:")
+    sex = input("Geschlecht (male or female):")
+    age = int(input("Alter:"))
+
+    subject_info = my_functions.build_person(first_name, last_name, sex, age)
 
     # Erstelle ein Experiment
-    experiment_info = my_functions.build_experiment("Heart Rate Study", "2024-03-19", supervisor_info, subject_info)
+    experiment_info = my_functions.build_experiment(name,datum, supervisor_info, subject_info)
+    print(experiment_info)
 
     # Drucke das Experimenten-Dictionary
     print("Experiment Details:")
     for key, value in experiment_info.items():
         print(f"{key}: {value}")
+
+
+    # Define student_details dictionary
+    student_details ={ 
+        "Experimentenname" : name, 
+        "Experimentendatum": datum, 
+        "Diagnostiker": supervisor_info,
+        "Versuchsperson": subject_info
+        } 
+
+    # Convert and write JSON object to file
+    with open("sample.json", "w") as outfile: 
+        json.dump(student_details, outfile)
